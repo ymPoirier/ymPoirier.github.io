126068a1185945d2b1d4765bcaed20f7b7f12930
Yves-Marie Poirier <yves-marie.poirier@francetv.fr>
2020-01-02 15:14:15 +0100

    size

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ var models = [{
         url: './assets/magnemite/scene.gltf',
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
